dec79a989f494bf0a425c72197d89588a20bacf9
DavidCdajud <142352460+DavidCdajud@users.noreply.github.com>
2024-09-23 22:37:28 -0500

    Creación ambiente desarrollo

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },  // Redirige la ruta base a /login
+  { path: 'login', component: LoginComponent },           // Muestra LoginComponent en /login
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
